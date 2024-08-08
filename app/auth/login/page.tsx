7ff1b7ea24cbd97d97c4cb7d647a7feffd133704
BryanVas24 <bryanvasquez2406@gmail.com>
2024-08-08 16:30:09 -0600

    creando carpeta de types y estilizando navBar

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+export default function LoginPage() {
+  return (
+    <>
+      <h1 className="text-5xl font-black text-davys-gray text-center">
+        Iniciar sesion
+      </h1>
+      <p className="text-2xl font-light text-davys-gray mt-5 text-center">
+        Para adquirir una motocileta {""}
+        <span className=" text-paynes-gray font-bold">
+          {" "}
+          inicia sesion en este formulario
+        </span>
+      </p>
+    </>
+  );
+}
--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -3,15 +3,33 @@ import Logo from "./Logo";
 
 export default function NavBar() {
   return (
-    <nav className="flex flex-col gap-10 md:flex-row justify-between bg-platinum">
+    <nav className="flex flex-col gap-10 md:flex-row justify-evenly items-center bg-platinum mb-5">
       <Link href={"/"}>
         <Logo />
       </Link>
 
-      <div>
-        <Link href={"/"}>Inicio</Link>
-        <Link href={"/motos"}>Motos</Link>
-        <Link href={"/contacto"}>Contacto</Link>
+      <div className="flex gap-5 flex-col md:flex-row  items-center text-xl font-medium">
+        <Link className="hover:text-davys-gray " href={"/"}>
+          Inicio
+        </Link>
+        <Link className="hover:text-davys-gray" href={"/motos"}>
+          Motos
+        </Link>
+        <Link className="hover:text-davys-gray" href={"/contacto"}>
+          Contacto
+        </Link>
+        <Link
+          className="border border-davys-gray bg-paynes-gray px-2 hover:bg-davys-gray text-alice-blue "
+          href={"/auth/login"}
+        >
+          Iniciar sesión
+        </Link>
+        <Link
+          className="border border-davys-gray bg-paynes-gray px-2 hover:bg-davys-gray text-alice-blue mb-5 md:mb-0 "
+          href={"/auth/register"}
+        >
+          Registrarse
+        </Link>
       </div>
     </nav>
   );
