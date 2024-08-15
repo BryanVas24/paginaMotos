c10d0b673d8c078b0fd3668744065f3218e9f025
BryanVas24 <bryanvasquez2406@gmail.com>
2024-08-15 13:34:25 -0600

    Agregando iconos de redes sociales

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
+import {
+  FaWhatsappSquare,
+  FaInstagramSquare,
+  FaFacebookSquare,
+} from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 export default function ContactoPage() {
   return (
     <main>
-      <section className="grid grid-cols-1 md:grid-cols-2">
+      <section className="grid grid-cols-1 md:grid-cols-2 ">
         <div>
-          <h1 className="text-5xl font-black text-davys-gray ml-4 mb-5 ">
+          <h1 className="text-5xl font-black text-davys-gray ml-4 mb-5">
             Contacta con nosotros
           </h1>
-          <p className="text-xl  text-davys-gray mx-7 text-justify">
+          <p className="text-xl text-davys-gray mx-7 text-justify">
             Estamos aquí para ayudarte. Si tienes alguna pregunta, sugerencia o
             necesitas asistencia, no dudes en comunicarte con nosotros. Nuestro
             equipo está disponible para brindarte la mejor atención posible.
@@ -16,11 +22,40 @@ export default function ContactoPage() {
             correo electrónico, o llamarnos directamente. ¡Tu opinión es muy
             importante para nosotros!
           </p>
-          <div>aca redes sociales</div>
+          <div className="flex space-x-4 ml-7 mt-10 justify-center gap-10 mb-5">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookSquare className="text-paynes-gray text-6xl transition-colors duration-300 hover:text-blue-600" />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaSquareXTwitter className="text-paynes-gray text-6xl transition-colors duration-300 hover:text-black" />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagramSquare className="text-paynes-gray text-6xl transition-colors duration-300 hover:text-pink-600" />
+            </a>
+            <a
+              href="https://wa.me/tu-numero"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaWhatsappSquare className="text-paynes-gray text-6xl transition-colors duration-300 hover:text-green-500" />
+            </a>
+          </div>
         </div>
         <div className="flex items-center justify-center">
           <Image
-            alt="Imagen de mecanico de motos"
+            alt="Imagen de mecánico de motos"
             src={"/contactImage1.jpg"}
             width={600}
             height={600}
